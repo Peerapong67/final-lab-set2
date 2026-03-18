79b60f32bd94085679b0dbb2222337f180f47abc
Peerapong67 <pr3kubim@gmail.com>
2026-03-18 13:25:52 +0700

    feat(frontend): add register, activity timeline, config.js , Delete nginx, edit docker-compose.yml

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  AUTH_URL:     'https://your-auth-service.up.railway.app',
+  TASK_URL:     'https://your-task-service.up.railway.app',
+  ACTIVITY_URL: 'https://your-activity-service.up.railway.app'
+};
