e1b7cc9275c2ed7a94919cccb91037f1b3ca8c2d
Peerapong67 <pr3kubim@gmail.com>
2026-03-18 16:27:47 +0700

    edit auth-url in config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL:     'https://auth-service-production-a40a.up.railway.app',
+  AUTH_URL:     'https://auth-service-production-3780.up.railway.app',
   TASK_URL:     'https://task-service-production-168f.up.railway.app',
   ACTIVITY_URL: 'https://activity-service-production-6e05.up.railway.app'
 };
